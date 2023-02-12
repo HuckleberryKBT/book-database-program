601fde075cab9ed22bd72f0816945fdbdeee4ea4
HuckleberryKen <110798624+HuckleberryKen@users.noreply.github.com>
2023-02-11 22:18:19 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,8 @@
+#Import models
+#main menu - add, search, analysis, exit, view
+#add books to the database
+#edit books
+#delete books
+#search function
+#data cleaning function
+#loop that runs the program
---
+++ models.py
@@ -0,0 +1,4 @@
+#create database
+#give it a name
+#create a model
+# title, author, date published, and price
