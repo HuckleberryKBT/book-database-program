8f1923ba32a0ee6410decabca9b80efb7deefa08
HuckleberryKen <110798624+HuckleberryKen@users.noreply.github.com>
2023-02-11 22:28:22 -0500

    added db model

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Import models
+from models import (Base, session, Book, engine)
 #main menu - add, search, analysis, exit, view
 #add books to the database
 #edit books
@@ -6,3 +6,7 @@
 #search function
 #data cleaning function
 #loop that runs the program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
